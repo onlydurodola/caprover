544adcae671cb8f0ed7cebe20ac126961123532b
onlydurodola <nowonlywork@gmail.com>
2025-07-22 13:24:07 +0100

    updated ansible h

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -172,6 +172,6 @@ resource "aws_vpc_endpoint" "ec2_messages" {
 resource "aws_route" "private_ssm_route" {
   count                  = length(aws_subnet.private)
   route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
+  destination_cidr_block = var.vpc_cidr
   nat_gateway_id         = aws_nat_gateway.nat.id
 }
